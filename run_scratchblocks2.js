4d68b780965510d0563249ef12197a107b100dc2
Rumanti <Rumanti@users.noreply.github.com>
2015-04-01 08:57:30 +0700

    Replace the code "indo" with "id"
    Fixing this silly mistake.

--- run_scratchblocks2.js
+++ run_scratchblocks2.js
@@ -1,3 +1,3 @@
-scratchblocks2.load_language(scratchblocks2._translations.indo);
+scratchblocks2.load_language(scratchblocks2._translations.id);
 scratchblocks2.parse();
 scratchblocks2.parse('code.blocks', {inline: true});
